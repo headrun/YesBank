39a9524008c979b6e818805d27cfe2a8713987f9
ranjan <ranjan@headrun.com>
2020-04-06 12:01:22 +0530

    #10 fixed npm

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ sudo apt install curl
 curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 sudo apt install nodejs
 npm i puppeteer
-nmp i puppeteer-extra
+npm i puppeteer-extra
 npm i node-datetime
 npm i puppeteer-extra-plugin-stealth
 npm install express --save
